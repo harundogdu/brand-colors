66ff5890cfb4aa6fca5398f89c694bcc228af250
Harun Doğdu <harundogdu06@gmail.com>
2022-01-09 19:56:50 +0300

    used react-router-dom

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
-import {store} from "redux/store";
+import { store } from "redux/store";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
---
+++ src/views/HomePage.js
@@ -0,0 +1,12 @@
+import Content from "components/Content";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <>
+      <Content />
+    </>
+  );
+};
+
+export default HomePage;
